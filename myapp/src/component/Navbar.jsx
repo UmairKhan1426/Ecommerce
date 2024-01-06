4a39bd18f59e9bdf2cb5f5be80ee8c302f4b1819
UmairKhan1426 <uk1985429@gmail.com>
2024-01-06 15:03:18 +0500

    Slider

--- myapp/src/component/Navbar.jsx
+++ myapp/src/component/Navbar.jsx
@@ -1,6 +1,26 @@
+import React, {useState} from 'react';
+import {BsChevronCompactLeft,BsChevronCompactRight} from 'react-icons/bs';
 const Navbar = ()=>{
+    const slides=[
+        {img:'/5650407.jpg'},
+        {img:'/5650410.jpg'},
+        {img:'/5650406.jpg'},
+        
+    ];
+    const [currentIndex, setcurrentIndex]= useState (0)
+    const prevSlide = () =>{
+        const isFirstSlide= currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length -1 : currentIndex - 1;
+        setcurrentIndex(newIndex);
+    }
+    const nextSlide = () =>{
+        const isLastSlide= currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setcurrentIndex(newIndex);
+
+    }
     return (
-        <div className="w-100 my-2 mx-40 bg-slate-200">
+        <div className="w-100 my-2 mx-40 bg-slate-200 ">
             <div className="mx-2
              flex"><a className="flex"><img src="https://img.icons8.com/?size=48&id=jWPjQhx0oPhE&format=png"/></a>
              <a href="https://www.education.com/" className="my-2">|Eduction.com</a>
@@ -9,8 +29,8 @@ const Navbar = ()=>{
                
                 <input className="my-2 mx-0 border-x-2 border-black border-y-2 rounded-2xl" type="search" placeholder="  Search here"></input>
              </form>
-             <button className="ml-80" >Login</button>
-             <button>|Logout</button>
+             <button className="ml-auto" >Login</button>
+             <button className="mr-0" >|Logout</button>
             
              </div>
              <nav>
@@ -27,7 +47,15 @@ const Navbar = ()=>{
                     </ul>
                 </div>
              </nav>
-            <div><img src="https://wallpaperaccess.com/full/5650407.jpg"/></div>
+    <div className='relative'><div className='duration-500 relative group'><img src={slides[currentIndex].img}/></div></div>
+<div className='group-hover:block absolute top-[30%] translate-y-30 mx-4 cursor-pointer'><BsChevronCompactLeft size={30} onClick={prevSlide}/></div>
+<div className='group-hover:block absolute top-[30%] translate-y-30 right-[11%] cursor-pointer'><BsChevronCompactRight size={30} onClick={nextSlide}/></div>
+<div className="text-5xl text-center h-8 bg-slate-200"><h1> Our learning library</h1></div>
+<div className="text-2xl text-center h-2 my-8 bg-slate-200"><h1> With 30,000+ digital and printable resources, kids can learn about any topic they’re curious about.</h1></div>
+<div class="pk" ><button className=" items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 rounded ">Browse the library</button></div>
+
+    
+
         
         </div>
     )
